38d5ad77176200c8cf1ea97fb6325df6cfb49557
Emin <ma1r57@163.com>
2022-03-30 04:31:00 +0800

    practice: 找出倒数第n个节点

--- src/common/linklist/linklist.go
+++ src/common/linklist/linklist.go
@@ -107,3 +107,17 @@ func findMiddleNode(root *ListNode) (*ListNode, *ListNode) {
 
 	return pre, last
 }
+
+// RemoveNthFromEnd 删除倒数第N个元素
+func RemoveNthFromEnd(head *ListNode, n int) *ListNode {
+	dummy := &ListNode{0, head}
+	first, second := head, dummy
+	for i := 0; i < n; i++ {
+		first = first.Next
+	}
+	for ; first != nil; first = first.Next {
+		second = second.Next
+	}
+	second.Next = second.Next.Next
+	return dummy.Next
+}
