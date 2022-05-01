807830055b9d48422b2f2d238bc9a20919cb1ece
Emin <ma1r57@163.com>
2022-05-01 23:33:40 +0800

    practice(string):字符串压栈解码

--- src/common/str/str.go
+++ src/common/str/str.go
@@ -1,1 +1,50 @@
 package str
+
+import "strconv"
+
+//link :https://leetcode-cn.com/problems/decode-string/solution/
+// 字符串解码v
+func decodeString(s string) string {
+	nums := make([]int, 0)
+	stack := make([]byte, 0)
+	i := 0
+	for ; i < len(s); i++ {
+		if isDigit(s[i]) {
+			j := i + 1
+			for isDigit(s[j]) {
+				j++
+			}
+
+			num, _ := strconv.Atoi(s[i:j])
+			nums = append(nums, num)
+			i = j - 1
+		} else if s[i] != ']' {
+			stack = append(stack, s[i])
+		} else {
+			var str, tmpStr string
+			for stack[len(stack)-1] != '[' {
+				str = string(stack[len(stack)-1]) + str
+				stack = stack[:len(stack)-1]
+			}
+			num := nums[len(nums)-1]
+			nums = nums[:len(nums)-1]
+			stack = stack[:len(stack)-1]
+			for num != 0 {
+				tmpStr += str
+				num--
+			}
+
+			stack = append(stack, tmpStr...)
+		}
+	}
+
+	return string(stack)
+}
+
+func isDigit(v byte) bool {
+	if v >= '0' && v <= '9' {
+		return true
+	}
+
+	return false
+}
--- src/common/str/str_test.go
+++ src/common/str/str_test.go
@@ -1,1 +1,25 @@
 package str
+
+import "testing"
+
+func Test_decodeString(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{name: "parse string", args: args{
+			s: "3[a2[c]]",
+		}, want: "accaccacc"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := decodeString(tt.args.s); got != tt.want {
+				t.Errorf("decodeString() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
