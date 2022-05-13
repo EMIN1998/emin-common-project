02fbf868360b681611ececd1e6c7727472b25b52
Emin <ma1r57@163.com>
2022-05-13 22:06:29 +0800

    practice(array):旋转图像

--- src/common/array/arrray.go
+++ src/common/array/arrray.go
@@ -374,6 +374,7 @@ func minPathSum(grid [][]int) int {
 	return resMap[m][n]
 }
 
+// ====================================================================================================================
 // link : https://leetcode-cn.com/problems/unique-paths/
 // 62. 不同路径
 func uniquePaths(m int, n int) int {
@@ -401,4 +402,22 @@ func uniquePaths(m int, n int) int {
 	}
 
 	return resMap[m-1][n-1]
+}
+
+// ====================================================================================================================
+// 48. 旋转图像
+// link:https://leetcode.cn/problems/rotate-image/
+func rotate(matrix [][]int) {
+	n := len(matrix)
+
+	for i := 0; i < n/2; i++ {
+		for j := 0; j < (n+1)/2; j++ {
+			tmp := matrix[i][j]
+			matrix[i][j] = matrix[n-j-1][i]
+			matrix[n-j-1][i] = matrix[n-i-1][n-j-1]
+			matrix[n-i-1][n-j-1] = matrix[j][n-i-1]
+			matrix[j][n-i-1] = tmp
+
+		}
+	}
 }
